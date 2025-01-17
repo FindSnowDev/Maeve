e2b2f282071437c7a6e3e586c8f38f37200e41ee
etherxp <okaytrismail@gmail.com>
2025-01-17 12:12:47 -0600

    commit

--- src/main/java/net/findsnow/MaeveBot.java
+++ src/main/java/net/findsnow/MaeveBot.java
@@ -1,12 +1,45 @@
 package net.findsnow;
 
-public class Main {
-	public MaeveBot() {
-		
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import net.findsnow.listeners.EventListener;
+
+import javax.security.auth.login.LoginException;
+
+public class MaeveBot {
+	private final Dotenv config;
+	private final ShardManager shardManager;
+
+	public MaeveBot() throws LoginException {
+		config = Dotenv.configure().load();
+		String token = config.get("TOKEN");
+
+		DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createLight(token);
+		builder.setStatus(OnlineStatus.ONLINE);
+		builder.setActivity(Activity.watching("My enemies..."));
+		builder.build();
+		shardManager = builder.build();
+
+		// REGISTER EVENT LISTENERS
+		shardManager.addEventListener(new EventListener());
 	}
 
+	public Dotenv getConfig() {
+		return config;
+	}
+
+	public ShardManager getShardManager() {
+		return shardManager;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		try {
+			MaeveBot maeveBot = new MaeveBot();
+		} catch (LoginException e) {
+			System.out.println("Failed to login to Discord. Goodbye...");
+		}
 	}
 }
--- src/main/java/net/findsnow/listeners/EventListener.java
+++ src/main/java/net/findsnow/listeners/EventListener.java
@@ -1,2 +1,26 @@
-package net.findsnow.listeners;public class EventListener {
+package net.findsnow.listeners;
+
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.Objects;
+
+public class EventListener extends ListenerAdapter {
+	@Override
+	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+		super.onGuildMemberJoin(event);
+	}
+
+	@Override
+	public void onMessageReactionAdd(MessageReactionAddEvent event) {
+		User user = event.getUser();
+		String emoji = event.getReaction().getEmoji().getAsReactionCode();
+		String channelMention = event.getChannel().getAsMention();
+		String jumpLink = event.getJumpUrl();
+
+		String message = user.getAsTag() + "reacted with " + emoji + " in " + channelMention + " | " + jumpLink;
+		Objects.requireNonNull(event.getGuild().getDefaultChannel()).asStandardGuildMessageChannel().sendMessage(message).queue();
+	}
 }
